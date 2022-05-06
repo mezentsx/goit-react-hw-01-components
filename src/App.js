5835db0f8e6009e4a9e9f975cf6cf12b83808d6b
Xeniya Mezentseva <mezentsxn@gmail.com>
2022-05-06 17:21:58 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Profile from './components/Profile/Profile';
-import user from './components/Profile/user.json';
+import user from './data/user.json';
 
 import Statistics from './components/Statistics/Statistics';
-import data from './components/Statistics/data.json';
+import data from './data/data.json';
 
 import FriendList from './components/FriendList/FriendList';
-import friends from './components/FriendList/friends.json';
+import friends from './data/friends.json';
 
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import transactions from './components/TransactionHistory/transactions.json';
+import transactions from './data/transactions.json';
 
 export default function App() {
     return (
